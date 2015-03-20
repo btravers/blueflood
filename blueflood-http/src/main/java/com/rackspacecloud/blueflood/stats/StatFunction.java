d9a9251e6b475b7f60690f555b03557681362ce5
btravers <btravers@insa-rennes.fr>
2015-03-20 16:10:22 +0100

    StatsFunction, derivative per minute

--- blueflood-http/src/main/java/com/rackspacecloud/blueflood/stats/StatFunction.java
+++ blueflood-http/src/main/java/com/rackspacecloud/blueflood/stats/StatFunction.java
@@ -28,7 +28,7 @@ public enum StatFunction {
 			Map<Long, Point> firstPoints = firstParam.getData().getPoints();
 			
 			for (Map.Entry<Long, Point> point : firstPoints.entrySet()) {
-				long timestamp = point.getKey() - (point.getKey()%60000);
+				long timestamp = point.getKey() - (point.getKey()%GRANULARITY);
 				
 				if (point.getValue().getData() instanceof SimpleNumber) {
 					values.put(timestamp, ((Point<SimpleNumber>) point.getValue()).getData().getValue().doubleValue());
@@ -48,7 +48,7 @@ public enum StatFunction {
 				Map<Long, Point> dataPoints = data.getData().getPoints();
 				
 				for (Map.Entry<Long, Point> point : dataPoints.entrySet()) {
-					long timestamp = point.getKey() - (point.getKey()%60000);
+					long timestamp = point.getKey() - (point.getKey()%GRANULARITY);
 					
 					Double tmp = values.get(timestamp);
 					if (tmp == null) {
@@ -93,7 +93,7 @@ public enum StatFunction {
 			Map<Long, Point> firstPoints = firstParam.getData().getPoints();
 			
 			for (Map.Entry<Long, Point> point : firstPoints.entrySet()) {
-				long timestamp = point.getKey() - (point.getKey()%60000);
+				long timestamp = point.getKey() - (point.getKey()%GRANULARITY);
 				
 				if (point.getValue().getData() instanceof SimpleNumber) {
 					values.put(timestamp, ((Point<SimpleNumber>) point.getValue()).getData().getValue().doubleValue());
@@ -113,7 +113,7 @@ public enum StatFunction {
 				Map<Long, Point> dataPoints = data.getData().getPoints();
 				
 				for (Map.Entry<Long, Point> point : dataPoints.entrySet()) {
-					long timestamp = point.getKey() - (point.getKey()%60000);
+					long timestamp = point.getKey() - (point.getKey()%GRANULARITY);
 					
 					Double tmp = values.get(timestamp);
 					if (tmp == null) {
@@ -155,27 +155,31 @@ public enum StatFunction {
 			Map<Long, Point> points = params.get(0).getData().getPoints();
 			
 			Number prev = null;
+			Long prevKey = null;
 			Points<SimpleNumber> resultPoints = new Points<SimpleNumber>();
 			for (Map.Entry<Long, Point> point : points.entrySet()) {
 				if (prev != null) {
+					double deltaT = (point.getKey() - prevKey)/GRANULARITY;
 					if (point.getValue().getData() instanceof SimpleNumber) {
 						Number tmp = ((Point<SimpleNumber>) point.getValue()).getData().getValue();
-						resultPoints.add(new Point<SimpleNumber>(point.getKey(), new SimpleNumber(tmp.doubleValue() - prev.doubleValue())));
+						resultPoints.add(new Point<SimpleNumber>(point.getKey(), new SimpleNumber((tmp.doubleValue() - prev.doubleValue()) / deltaT)));
 						prev = tmp;
 					} else if (point.getValue().getData() instanceof BasicRollup) {
 						if (((Point<BasicRollup>) point.getValue()).getData().getAverage().isFloatingPoint()) {
 							Number tmp = ((Point<BasicRollup>) point.getValue()).getData().getAverage().toDouble();
-							resultPoints.add(new Point<SimpleNumber>(point.getKey(), new SimpleNumber(tmp.doubleValue() - prev.doubleValue())));
+							resultPoints.add(new Point<SimpleNumber>(point.getKey(), new SimpleNumber((tmp.doubleValue() - prev.doubleValue()) / deltaT)));
 							prev = tmp;
 						}else {
 							Number tmp = ((Point<BasicRollup>) point.getValue()).getData().getAverage().toLong();
-							resultPoints.add(new Point<SimpleNumber>(point.getKey(), new SimpleNumber(tmp.doubleValue() - prev.doubleValue() )));
+							resultPoints.add(new Point<SimpleNumber>(point.getKey(), new SimpleNumber((tmp.doubleValue() - prev.doubleValue()) / deltaT)));
 							prev = tmp;
 						}
 					} else {
 						throw new TargetTypeException("Stats requests expect SimpleNumber data instead of " + point.getClass());
 					}
+					prevKey = point.getKey();
 				} else {
+					prevKey = point.getKey();
 					if (point.getValue().getData() instanceof SimpleNumber) {
 						prev = ((Point<SimpleNumber>) point.getValue()).getData().getValue();
 					} else if (point.getValue().getData() instanceof BasicRollup) {
@@ -198,6 +202,7 @@ public enum StatFunction {
 	
 	private String function;
 	private static final Map<String, StatFunction> stringToEnum = new HashMap<String, StatFunction>();
+	private final static int GRANULARITY = 60000;
 	
 	private StatFunction(String s) {
 		this.function = s;
